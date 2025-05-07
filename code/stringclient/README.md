4c70d8ac48ce624fa8b8504e515475bd1c3b9e3a
Raphael Steinhöfler <r.steinhoefler@gmail.com>
2025-05-07 12:22:14 +0200

    fix client script ctrl c issues

--- code/stringclient/README.md
+++ code/stringclient/README.md
@@ -3,4 +3,6 @@
 To use this client script run it via command line with `python client.py PLACE.RASPBERRYPI.IP.HERE`. Optionally, you can also set the host raspberry pi IP inside the python script and run it without arguments.  
 At the start of the script, it will connect to the host.  
 
-In order to send a string to the host, simply enter your string and press enter. If there is any response from the host, it will be displayed in the output of the script.
+In order to send a string to the host, simply enter your string and press enter. If there is any response from the host, it will be displayed in the output of the script.  
+
+Exit the script by entering the message `bye`.
--- code/stringclient/client.py
+++ code/stringclient/client.py
@@ -1,25 +1,46 @@
 import socket
 import threading
 import sys
+import signal
+import select
 
 running = 1
 
+#ctrl+c handling
+def stop_program(signum, frame):
+    global running
+    running = 0
+    #client_socket.shutdown(socket.SHUT_WR)
+    print("detected ctrl+c, stopping program...")
+
 def sender_interface(client_socket):
     global running
-    message = input()  # take input
+    try:
+        message = input()  # again take input
+    except (KeyboardInterrupt, EOFError):
+        stop_program(0,0)
 
     while running:
         if message.lower().strip() == 'bye':
             running = 0
         client_socket.send(message.encode())  # send message
-        message = input()  # again take input
+        try:
+            message = input()  # again take input
+        except (KeyboardInterrupt, EOFError):
+            stop_program(0,0)
+    print("ended sender thread!")
 
 def receiver_interface(client_socket):
     global running
+    client_socket.settimeout(1)
     while running:
-        data = client_socket.recv(1024).decode()  # receive response
-
-        print('Received from server: ' + data)  # show in terminal
+        try:
+            data = client_socket.recv(1024)  # receive response
+            data = data.decode()
+            print('Received from server: ' + data)  # show in terminal
+        except socket.timeout:
+            continue
+    print("ended receiver thread!")
 
 
 def client_program(host):
@@ -30,6 +51,7 @@ def client_program(host):
 
     sender_thread = threading.Thread(target=sender_interface, args=[client_socket])
     receiver_thread = threading.Thread(target=receiver_interface, args=[client_socket])
+    receiver_thread.daemon=True
 
     for thethread in [sender_thread, receiver_thread]:
         thethread.start()
@@ -45,5 +67,9 @@ if __name__ == '__main__':
         host = sys.argv[1]
     else:
         host = "ENTER.DEFAULT.HOST_IP.HERE"
+
+    #ctrl+c handling    
+    signal.signal(signal.SIGINT, stop_program)
     
+    print(f"connecting to host {host}")
     client_program(host)
